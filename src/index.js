a85a7a58ab883944e5f0881adfa20d1bca6536b0
RudraIIT <b22125@students.iitmandi.ac.in>
2025-01-30 09:47:39 +0530

    nmesg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import path from "path";
+import { fileURLToPath } from 'url';
 import { HfInference } from "@huggingface/inference"
 import cors from 'cors';
 import dotenv from 'dotenv';
